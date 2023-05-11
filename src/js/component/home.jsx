0aba1c1cd97d1b05979250a65ae36bcb52e6ae84
DevMigue <127422296+DevMigue@users.noreply.github.com>
2023-05-11 23:55:55 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+
+	const [color, setColor]= useState("red");
+	const [purple, setPurple]= useState(false);
+
 	return (
+		<>
 		<div className="semaforo">
-  			<div className="red"></div>
-  			<div className="yellow"></div>
-  			<div className="green"></div>
+  			<div className={color=="red" ? "red light" : "red"} onClick={() => setColor("red")}></div>
+  			<div className={color=="yellow" ? "yellow light" : "yellow" } onClick={() => setColor("yellow")}></div>
+  			<div className={color=="green" ? "green light" : "green"} onClick={() => setColor("green")}></div>
+  			<div className={purple ? color=="purple" ? "purple light" : "purple" : "purple d-none"} onClick={() => setColor("purple")}></div>
 		</div>
+
+		<div>
+			<button className="button" onClick={() => purple == false ? setPurple(true) : setPurple(false)}>Semaforo Purple</button>
+		</div>
+		</>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -11,25 +11,3 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
-
-const red = document.querySelector('.red');
-const yellow = document.querySelector('.yellow');
-const green = document.querySelector('.green');
-
-red.addEventListener('click', function() {
-  red.classList.add('light');
-  yellow.classList.remove('light');
-  green.classList.remove('light');
-});
-
-yellow.addEventListener('click', function() {
-  yellow.classList.add('light');
-  red.classList.remove('light');
-  green.classList.remove('light');
-});
-
-green.addEventListener('click', function() {
-  green.classList.add('light');
-  red.classList.remove('light');
-  yellow.classList.remove('light');
-});
