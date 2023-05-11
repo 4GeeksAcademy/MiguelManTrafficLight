5f73d51d8130abefc98df3aeed5755be69823b1c
DevMigue <127422296+DevMigue@users.noreply.github.com>
2023-05-11 02:19:28 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
@@ -8,5 +8,28 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+
+red.addEventListener('click', function() {
+  red.classList.add('light');
+  yellow.classList.remove('light');
+  green.classList.remove('light');
+});
+
+yellow.addEventListener('click', function() {
+  yellow.classList.add('light');
+  red.classList.remove('light');
+  green.classList.remove('light');
+});
+
+green.addEventListener('click', function() {
+  green.classList.add('light');
+  red.classList.remove('light');
+  yellow.classList.remove('light');
+});
